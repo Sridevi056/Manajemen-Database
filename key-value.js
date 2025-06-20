6cd580c87af77e071b65d2deb8437e472edcbcc8
Sridevi056 <147229706+Sridevi056@users.noreply.github.com>
2025-06-20 17:32:20 +0800

    Add files via upload

---
+++ key-value.js
@@ -0,0 +1,7 @@
+db.produk_kv.insertMany([
+  { _id: "produk:101", value: { nama: "Laptop", harga: 7500000, stok: 10 } },
+  { _id: "produk:102", value: { nama: "Mouse", harga: 150000, stok: 50 } },
+  { _id: "produk:103", value: { nama: "Keyboard", harga: 300000, stok: 30 } },
+  { _id: "produk:104", value: { nama: "Monitor", harga: 2000000, stok: 15 } },
+  { _id: "produk:105", value: { nama: "Printer", harga: 1800000, stok: 7 } }
+])
---
+++ wide-column.js
@@ -0,0 +1,37 @@
+db.mahasiswa_wc.insertMany([
+  {
+    _id: 1,
+    nama: "Andi",
+    jurusan: "Informatika",
+    angkatan: 2021,
+    ipk: 3.5
+  },
+  {
+    _id: 2,
+    nama: "Budi",
+    jurusan: "Sistem Informasi",
+    angkatan: 2020,
+    ipk: 3.2
+  },
+  {
+    _id: 3,
+    nama: "Citra",
+    jurusan: "Teknik Komputer",
+    angkatan: 2022,
+    ipk: 3.75
+  },
+  {
+    _id: 4,
+    nama: "Dewi",
+    jurusan: "Informatika",
+    angkatan: 2023,
+    ipk: 3.6
+  },
+  {
+    _id: 5,
+    nama: "Eko",
+    jurusan: "RPL",
+    angkatan: 2021,
+    ipk: 3.4
+  }
+])
